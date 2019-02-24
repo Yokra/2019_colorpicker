a1659bf587603c327882beb903c4ed4780447eae
yokra <yordanka_krasteva@mail.bg>
2019-02-24 18:03:37 +0100

    creating the green rectangle

--- colorpicker.js
+++ colorpicker.js
@@ -1,4 +1,42 @@
 "use strict";
+const image = document.getElementById("imageCanvas");
+const zoom = document.getElementById("zoomCanvas");
+let ctx = image.getContext('2d');
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init()
+{
+    let img = new Image();
+    img.src = "cat.jpg";
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);};
+
+}
+
+imageCanvas.addEventListener("mousemove", mouseMoved);
+function mouseMoved(event){
+    console.log(event);
+       let x = event.offsetX;
+       let y = event.offsetY;
+
+       //the rectangle appears
+    ctx.strokeRect(x, y, 20, 20,);
+    ctx.strokeStyle="lightgreen";
+
+       console.log(y,x);
+
+    /*let pixel = ctx.getImageData(x, y, 1, 1);
+    let data = pixel.data;
+    let rgba = 'rgba(' + data[0] + ', ' + data[1] +
+        ', ' + data[2] + ', ' + (data[3] / 255) + ')';
+    imageCanvas.style.background =  rgba;
+    imageCanvas.textContent = rgba;*/
+}
+
+
+
+
 
 // 🎁 Here you go! 🎁
 function showColorInfo( rgb ) {
